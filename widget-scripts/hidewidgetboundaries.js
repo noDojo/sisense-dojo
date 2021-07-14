46aaa190c27baedd1b6bd8a596e9ece8c3e3209d
noDojo <nulldojo@gmail.com>
2021-07-14 00:46:40 -0400

    add widget-scripts directory and hidewidgetboundaries.js

---
+++ widget-scripts/hidewidgetboundaries.js
@@ -0,0 +1,19 @@
+/*  Tested on Version: L2021.3.0
+
+    Removes widget box-shadow styles to make the widget
+    appear as if it doesn't have boundaries.
+
+    TODO: The same result can probably be achieved with fewer elements having styles applied.
+*/
+
+const _hideWidgetBoundariesStyles = {
+    'box-shadow': 'none'
+};
+
+
+widget.on('refreshed', function (w) {
+    $(element).css(_hideWidgetBoundariesStyles);
+    $(element).parent().css(_hideWidgetBoundariesStyles);
+    $(element).parent().parent().css(_hideWidgetBoundariesStyles);
+    $('div.dashboard-layout-subcell-vertical').css(_hideWidgetBoundariesStyles);
+});
